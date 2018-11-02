79a6f03e333fff41d7124709ee9225307516f717
laura.chacon <laurachacon92@gmail.com>
2018-11-02 16:27:52 +0100

    Add overlay at photos to show username and caption when hovering on each one

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Columns from 'react-columns';
 import Photo from './Photo';
 
 const PhotosContainer = styled.div`
-    flex: 1,
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default class Gallery extends Component {
@@ -15,27 +18,38 @@ export default class Gallery extends Component {
   }
 
   componentDidMount() {
-    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&tags=nyc&per_page=5&page=1&format=json&nojsoncallback=1`;
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&tags=nyc&per_page=20&page=1&format=json&content_type=1&nojsoncallback=1&extras=count_comments,count_faves,url_o&sort=interestingness-desc&extras=url_o`;
     fetch(url)
       .then(response => response.json())
       .then((jsonResponse) => {
+        console.log('jsonResponse',jsonResponse)
         const picArray = jsonResponse.photos.photo.map(pic => ({
           title: pic.title,
           owner: pic.owner,
-          imageUrl: `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`,
+          id: pic.id,
+          imageUrl: pic.url_o,
         }));
         this.setState({ photos: picArray });
       })
-      .bind(this);
+      .catch(error => console.error(error));
   }
 
   render() {
     const { photos } = this.state;
+    const queries = [{
+      columns: 1,
+      query: 'min-width: 200px',
+    }, {
+      columns: 2,
+      query: 'min-width: 500px',
+    }];
     return (
       <div>
         <h1>Gallery</h1>
         <PhotosContainer>
-          {photos.map(photo => <Photo photo={photo} key={photo.title} />)}
+          <Columns queries={queries}>
+            {photos.map(photo => <Photo photo={photo} key={photo.id} />)}
+          </Columns>
         </PhotosContainer>
       </div>
     );
--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -3,10 +3,58 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const PhotoContainer = styled.div`
+  margin: 10px;
+  position: relative;
 
 `;
 
+const EmptyImage = styled.div`
+width: 100%;
+height: 250px;
+background-color: rgba(64,64,64,0.3);
+`;
+
+const PhotoImage = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 2px;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: ${props => props.opacity};
+  transition: .5s ease;
+`;
+
+const PhotoInformation = styled.div`
+  width: 100%;
+  position: absolute;
+  bottom: 40%;
+  left: 50%;
+  color: white;
+  font-size: 20px;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+`;
+
 const OwnerText = styled.div`
+  cursor: pointer;
+  height: 20px;
+`;
+
+const TitleText = styled.div`
+  height: 20px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 
 `;
 
@@ -15,7 +63,12 @@ export default class Photo extends Component {
     super();
     this.state = {
       username: '',
+      hovering: false,
     };
+
+    this.handleOnClickUsername = this.handleOnClickUsername.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
   }
 
   componentDidMount() {
@@ -26,16 +79,59 @@ export default class Photo extends Component {
       .then((jsonResponse) => {
         this.setState({ username: jsonResponse.person.username._content });
       })
-      .bind(this);
+      .catch(error => console.error(error));
+  }
+
+  onMouseEnter() {
+    this.setState({ hovering: true });
+  }
+
+  onMouseLeave() {
+    this.setState({ hovering: false });
+  }
+
+  handleOnClickUsername() {
+    const { photo } = this.props;
+    window.open(`http://flickr.com/photos/${photo.owner}/${photo.id}`, '_newtab');
+  }
+
+  renderImage() {
+    const { photo } = this.props;
+    if (photo.imageUrl !== undefined) {
+      return <PhotoImage src={photo.imageUrl} />;
+    }
+    return null;
+  }
+
+  renderOverlay() {
+    const { photo } = this.props;
+    const { username, hovering } = this.state;
+    if (photo.imageUrl !== undefined) {
+      return (
+        <Overlay opacity={hovering ? 1 : 0}>
+          <PhotoInformation>
+            <TitleText>{photo.title}</TitleText>
+            <OwnerText onClick={this.handleOnClickUsername}>
+              by
+              {username}
+            </OwnerText>
+          </PhotoInformation>
+        </Overlay>
+      )
+    }
+    return null;
   }
 
   render() {
     const { photo } = this.props;
-    const { username } = this.state;
     return (
-      <PhotoContainer key={photo.owner}>
-        <img alt={photo.title} src={photo.imageUrl} />
-        <OwnerText>{username}</OwnerText>
+      <PhotoContainer
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+        key={photo.id}
+      >
+        {this.renderImage()}
+        {this.renderOverlay()}
       </PhotoContainer>
     );
   }
@@ -45,6 +141,7 @@ Photo.propTypes = {
   photo: PropTypes.shape({
     title: PropTypes.string,
     owner: PropTypes.string,
+    id: PropTypes.string,
     imageUrl: PropTypes.string,
   }),
 };
